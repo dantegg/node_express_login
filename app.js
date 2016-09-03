0c977c208d55df3df972c5ff93d349d780732ec4
dantegg <luroufan327@hotmail.com>
2016-09-03 10:11:58 +0800

    upload login module

--- app.js
+++ app.js
@@ -7,7 +7,25 @@ var multer = require('multer')
 
 var app = express()
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(cookieParser())
 app.use(bodyParser.json())
+app.use(expressSession({
+    secret:'secret',
+    resave:true,
+    saveUninitialized:true,
+    cookie:{
+        maxAge:1000*60*10
+    }
+}))
+
+
+app.use(function(req, res, next){
+    res.locals.user = req.session.user;
+    var err = req.session.error;
+    res.locals.message = '';
+    if (err) res.locals.message = '<div style="margin-bottom: 20px;color:red;">' + err + '</div>';
+    next();
+});
 app.set('view engine','ejs')
 //app.engine( '.html', require( 'ejs' ).__express );
 app.set('port',process.env.PORT||3000)
@@ -15,10 +33,34 @@ app.set('port',process.env.PORT||3000)
 app.use(express.static(__dirname+'/public'))
 
 
-app.get('/',function (req,res) {
+app.get('/login',function (req,res) {
     res.render('login')
 })
 
+
+app.post('/login',function (req,res) {
+    var user = {
+        username:'dantegg',
+        password:'dantegg'
+    }
+    console.log(req.body)
+    if(req.body.username == user.username && req.body.password == user.password){
+        req.session.user = user
+        res.redirect('myspace')
+    }else{
+        req.session.error = '用户名或密码不正确'
+        res.send(404)
+    }
+})
+
+app.get('/myspace',function(req,res){
+    if(req.session.user){
+        res.render('myspace');
+    }else{
+        req.session.error = "请先登录"
+        res.redirect('login');
+    }
+});
 
 
 //page 404
